83376f65e9e3d7f3cc177ea1cf7a594621bd8854
Aleksandr <alx.panteleev@gmail.com>
2021-03-06 09:13:28 +0100

    query categories.js

--- 07-mongodb-mongoose/02-rest-api/controllers/categories.js
+++ 07-mongodb-mongoose/02-rest-api/controllers/categories.js
@@ -1,3 +1,13 @@
-module.exports.categoryList = async function categoryList(ctx, next) {
-  ctx.body = {categories: []};
+const categoryModel = require('../models/Category');
+
+module.exports.categoryList = async function categoryList(ctx) {
+  try {
+    const categories = await categoryModel.find({});
+    ctx.body = {categories};
+  } catch (e) {
+    ctx.status = 500;
+    ctx.body = {
+      message: e.message,
+    };
+  }
 };
